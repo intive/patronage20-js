6ea6939b1495c2f5c079a5af3d65c8366fdade26
unknown <kamil.jalowiczor@gmail.com>
2020-03-06 23:42:07 +0100

    chore: add /authors and /authors/:id routes

--- src/app.js
+++ src/app.js
@@ -39,5 +39,12 @@ module.exports = function ({ port }) {
       prefix: '.well-known'
     })
 
+
+    app.register(require('./routes/catch-all/catch-all-api-404'), {
+      prefix: '/api'
+    });
+  
+    app.register(require('./routes/catch-all/catch-all'));
+    
   return app
 }
