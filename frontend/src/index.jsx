6e949c9484526809e1c9e3007332c9800a1bfab3
unknown <kamil.jalowiczor@gmail.com>
2020-03-06 23:42:07 +0100

    chore: add /authors and /authors/:id routes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,16 +4,12 @@ import './index.css'
 import { StoreContext } from 'redux-react-hook'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import store from './store'
-import Home from './views/Home.jsx'
-import KarolCard from './components/Authors/KarolCard'
+import App from './app'
 
 ReactDOM.render(
   <BrowserRouter>
     <StoreContext.Provider value={store}>
-      <Switch>
-        <Route path='/karol' component={KarolCard} />
-        <Route path='/' component={Home} />
-      </Switch>
+      <App />
     </StoreContext.Provider>
   </BrowserRouter>,
   document.getElementById('root')
