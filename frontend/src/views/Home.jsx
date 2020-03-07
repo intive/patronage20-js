4d0181481538f233566897e38868fba2b3490aef
MartaJaszewska <jaszewska.marta@gmail.com>
2020-03-07 14:26:51 +0100

    chore: add @material-ui/icons and react-particles-js library

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'redux-react-hook'
-import actionTypes from '../common/constants/actionTypes'
-import SampleCard from '../components/SampleCard'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'redux-react-hook';
+import actionTypes from '../common/constants/actionTypes';
+import SampleCard from '../components/SampleCard';
+import AboutMarta from '../components/AboutMarta';
 
-export default function Home () {
-  const dispatch = useDispatch()
+export default function Home() {
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch({ type: actionTypes.HOME_LOADED_ACTION })
-  }, [])
+    dispatch({ type: actionTypes.HOME_LOADED_ACTION });
+  }, []);
   return (
     <div>
-      <h1>Patronage 2020 JS!</h1>
-      <SampleCard />
+      <AboutMarta />
     </div>
-  )
+  );
 }
