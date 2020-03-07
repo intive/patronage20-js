9ef049b05f275c94a043fcdc227b41a006f45995
MartaJaszewska <jaszewska.marta@gmail.com>
2020-03-07 14:26:51 +0100

    chore: add @material-ui/icons and react-particles-js library

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -6,9 +6,11 @@ import { Link } from 'react-router-dom'
 
 export default function Home () {
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch({ type: actionTypes.HOME_LOADED_ACTION })
   }, [])
+
   return (
     <div>
       <h1>Patronage 2020 JS!</h1>
