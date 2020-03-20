2a841ac19242489f4f112ba4f2c522b254c13648
MartaJaszewska <jaszewska.marta@gmail.com>
2020-03-20 14:33:20 +0100

    feat: [PATRON2020-96] add WarningSnackbar component with styling

--- frontend/src/views/Layout.jsx
+++ frontend/src/views/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Grid, makeStyles } from '@material-ui/core'
 import Navigation from '../components/Layout/Navigation'
-
+import WarningSnackbar from '../components/Snackbar'
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100vh'
@@ -16,6 +16,7 @@ const Layout = ({ children }) => {
   const classes = useStyles()
   return (
     <Grid container maxwidth='xs' className={classes.root} data-testid='dashboard-id'>
+      <WarningSnackbar />
       <Navigation />
       <Grid container maxwidth='xs' className={classes.content}>
         {children}
