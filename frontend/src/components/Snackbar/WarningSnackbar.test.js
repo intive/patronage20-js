35bec1c36bcbc616c1f0feb4bcedf13637a9ec1f
MartaJaszewska <jaszewska.marta@gmail.com>
2020-03-25 20:47:00 +0100

    test: [PATRON202-96] added axios-mock-adapter and tried testing

--- frontend/src/components/Snackbar/WarningSnackbar.test.js
+++ frontend/src/components/Snackbar/WarningSnackbar.test.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { cleanup } from '@testing-library/react'
+import { create } from 'react-test-renderer'
 import WarningSnackbar from './index'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+const mock = new MockAdapter(axios, { delayResponse: 5000 })
+
+const timer = (callback) => {
+  console.log('Ready....go!')
+  setTimeout(() => {
+    callback()
+  }, 1000)
+}
+jest.useFakeTimers()
 
 afterEach(cleanup)
+describe('WarningSnackbar component', () => {
+  it('Matches the snapshot', () => {
+    const snackbar = create(<WarningSnackbar />)
+    expect(snackbar.toJSON()).toMatchSnapshot()
+  })
 
-it('should take a snapshot', () => {
-  const { asFragment } = render(<WarningSnackbar />)
-  expect(asFragment(<WarningSnackbar />)).toMatchSnapshot()
+  it('check response', () => {
+    const snackbar = create(<WarningSnackbar />)
+
+    mock.onGet('/.well-known/health-check').reply(200, { message: 'OK' })
+
+    timer(axios.get('/.well-known/health-check').then(response => {
+      console.log(response.data)
+    }), 5000)
+  })
 })
