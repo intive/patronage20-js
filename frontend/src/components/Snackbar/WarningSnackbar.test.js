e1ea6c2a34fb7500dad22d653eba220230e85b46
MartaJaszewska <jaszewska.marta@gmail.com>
2020-03-26 19:12:39 +0100

    test: [PATRON2020-96] added mock state

--- frontend/src/components/Snackbar/WarningSnackbar.test.js
+++ frontend/src/components/Snackbar/WarningSnackbar.test.js
@@ -7,14 +7,6 @@ import MockAdapter from 'axios-mock-adapter'
 
 const mock = new MockAdapter(axios, { delayResponse: 5000 })
 
-const timer = (callback) => {
-  console.log('Ready....go!')
-  setTimeout(() => {
-    callback()
-  }, 1000)
-}
-jest.useFakeTimers()
-
 afterEach(cleanup)
 describe('WarningSnackbar component', () => {
   it('Matches the snapshot', () => {
@@ -23,12 +15,24 @@ describe('WarningSnackbar component', () => {
   })
 
   it('check response', () => {
+    jest.useFakeTimers()
     const snackbar = create(<WarningSnackbar />)
+
+    const setState = jest.fn()
+    const useStateSpy = jest.spyOn(React, 'useState')
+    useStateSpy.mockImplementation((init) => [init, setState])
 
     mock.onGet('/.well-known/health-check').reply(200, { message: 'OK' })
 
-    timer(axios.get('/.well-known/health-check').then(response => {
-      console.log(response.data)
-    }), 5000)
+    axios.get('/.well-known/health-check').then(response => {
+      console.log(response)
+    })
+    expect(setState).toHaveBeenCalledTimes(0) // Success!
+
+    jest.advanceTimersByTime(5000)
+    axios.get('/.well-known/health-check').then(response => {
+      console.log(response)
+    })
+    // expect(setState).toHaveBeenCalledTimes(2)
   })
 })
